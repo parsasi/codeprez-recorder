eac3e8037c0d2bb8bfd56386e0e8c6f657222e52
parsasi <parsasi@rocketmail.com>
2021-01-16 21:25:39 +0330

    download and hash helpers created as utility functions

---
+++ code-prez/src/helpers/hash.js
@@ -0,0 +1,14 @@
+c(content) => {
+    let hashed = 0;
+    if (content.length == 0) {
+        return hashed;
+    }
+    for (let i = 0; i < content.length; i++) {
+        let char = content.charCodeAt(i);
+        hashed = ((hashed<<5)-hashed)+char;
+        hashed = hashed & hashed; // Convert to 32bit integer
+    }
+    return hashed;
+}
+
+export default hash
